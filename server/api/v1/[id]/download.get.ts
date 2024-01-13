b16d093f1e2fc4fb0b3e1317de018d771fd808dc
DecDuck <declanahofmeyr@gmail.com>
2024-01-13 20:33:11 +1100

    md5 hashes instead of base64 encoding

--- server/api/v1/[id]/download.get.ts
+++ server/api/v1/[id]/download.get.ts
@@ -1,15 +1,33 @@
 import { GetObjectCommand, HeadObjectCommand } from "@aws-sdk/client-s3";
-import { client } from "~/server/client";
+import { client, getKeyFromHash, hashKey } from "~/server/client";
 
 export default defineEventHandler(async (h3) => {
   const id = h3.context.params?.id;
   if (!id) {
     return createError({ statusCode: 400, statusMessage: "invalid id" });
   }
-  const key = atob(id);
+  if (id.endsWith("==")) {
+    const key = atob(id);
+    sendRedirect(h3, `/api/v1/${hashKey(key)}/download`, 301);
+    return;
+  }
+  const bucket = process.env.BUCKET;
+  if (!bucket) {
+    return createError({
+      statusCode: 500,
+      statusMessage: "BUCKET not set",
+    });
+  }
+  const key = await getKeyFromHash(id, bucket);
+  if (!key) {
+    return createError({
+      statusCode: 404,
+      statusMessage: "object with key not found",
+    });
+  }
   const cmd = new GetObjectCommand({
     Key: key,
-    Bucket: process.env.BUCKET,
+    Bucket: bucket,
   });
   let response;
   try {
@@ -17,8 +35,8 @@ export default defineEventHandler(async (h3) => {
   } catch (e) {
     return createError({
       statusCode: 404,
-      statusMessage: "object not found"
-    })
+      statusMessage: "object not found",
+    });
   }
   appendHeader(h3, "Content-Disposition", `inline; filename="${key}"`);
   appendHeader(h3, "Content-Length", `${response.ContentLength}`);
--- server/client.ts
+++ server/client.ts
@@ -1,7 +1,35 @@
 import * as AWS from "@aws-sdk/client-s3";
+import { Md5 } from "ts-md5";
 
+// @ts-ignore
 export const client = new AWS.S3({
-  credentials: { accessKeyId: process.env.ACCESS_ID, secretAccessKey: process.env.ACCESS_KEY },
+  credentials: {
+    accessKeyId: process.env.ACCESS_ID,
+    secretAccessKey: process.env.ACCESS_KEY,
+  },
   endpoint: process.env.ENDPOINT,
-  region: "fuck-if-i-care"
+  region: "fuck-if-i-care",
 });
+
+export const getKeyFromHash = async (hash: string, bucket: string) => {
+  const cmd = new AWS.ListObjectsV2Command({ Bucket: bucket });
+  const response = await client.send(cmd);
+  const objects = response.Contents;
+  if (objects == null) {
+    return null;
+  }
+  const objectIndex = objects?.findIndex((o) => {
+    if (o.Key == null) {
+      return false;
+    }
+    return hashKey(o.Key) == hash;
+  });
+  if (objectIndex == -1) {
+    return null;
+  }
+  return objects[objectIndex].Key;
+};
+
+export const hashKey = (key: string) => {
+  return Md5.hashStr(key);
+};
